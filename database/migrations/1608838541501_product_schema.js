af183f8d1b4714aa65de7952b1269d5cea4aeed2
lazaroa1 <lazaroalves36@gmail.com>
2021-02-09 19:05:40 -0300

    category and products

--- database/migrations/1608838541501_product_schema.js
+++ database/migrations/1608838541501_product_schema.js
@@ -5,7 +5,7 @@ const Schema = use('Schema')
 
 class ProductSchema extends Schema {
   up () {
-    this.create('products', (table) => {
+    this.create('products', table => {
       table.increments()
       table.strin('name', 200)
       table.string(image_id).unsigned()
@@ -35,9 +35,25 @@ class ProductSchema extends Schema {
       .inTable('products')
       .onDelete('cascade')
     })
+
+    this.create('category_products', table => {
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+      table
+      .foreign('product_id')
+      .reference('id')
+      .inTable('products')
+      .onDelete('cascade')
+      table
+      .foreign('category_id')
+      .reference('id')
+      .inTable('categories')
+      .onDelete('cascade')
+    }) 
   }
 
   down () {
+    this.drop('category_products')
     this.drop('image_product')
     this.drop('products')
   }
